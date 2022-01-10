651de92cae17f84e9cfd31f48313cc9a42f09a17
Esteve <estevesegura@protonmail.com>
2022-01-10 18:24:59 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const randomFloat = require('./useCases/randomFloat');
+
+const PORT = process.env.PORT || 3000;
+app.use(morgan(`:method :url port=${PORT} :status :res[content-length] - :response-time ms`))
+
+app.get('/api/v1/number', (req,res) => {
+  const value = randomFloat.randomFloat();
+  res.json({value});
+})
+
+app.listen(PORT, () => {
+  console.log(`Randomito API running on port ${PORT}`)
+})
